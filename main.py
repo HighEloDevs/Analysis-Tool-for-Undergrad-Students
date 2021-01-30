9d2fc5a4b6464573de54ffb07d4e4c6e8e7873d5
Leo Eiji <leoeiji1234@gmail.com>
2021-01-30 17:59:47 -0300

    Dark Mode added

--- main.py
+++ main.py
@@ -15,21 +15,26 @@ Created on Fri Jan 29 11:59:27 2021
 import sys
 from src.MainWindow import MainWindow
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import *
+from PyQt5 import QtWidgets, QtGui, QtCore
+import qdarkstyle
+
 
 def main():
+    # Initializing App
     app = QApplication(sys.argv)
+    
+    # Setting Style
+    app.setStyle('Fusion')
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
+    
+    # Opening Main Window
     win = MainWindow()
     win.show()
     sys.exit(app.exec_())
     
 if __name__ == "__main__":
     main()      
-
-
-
-
-
-
 
 # =============================================================================
 # f = lambda x: x**3 + x**2 + x + 10
